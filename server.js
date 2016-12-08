58d4ea6be96c7a5f268ba332db55f1fec3b35d99
aditya2337 <aditya.n@gai.co.in>
2016-12-08 21:42:23 +0530

    modifying server file for heroku deploy

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 
 // create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('https://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log('Listning port at 3000');
 })
